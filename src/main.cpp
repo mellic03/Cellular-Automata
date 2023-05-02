be5a868a19390cf094321bbcd21825cd542d4b2f
Michael Ellicott <mellic03@gmail.com>
2023-05-02 19:06:08 +1000

    Worms

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 
 int main(int argc, char **argv)
 {
-    Renderer ren;
+    Renderer ren(500, 500, 1200, 1200);
+    System sys;
+    Worms r1;
 
     auto a = SDL_GetTicks();
     auto b = SDL_GetTicks();
@@ -15,10 +17,12 @@ int main(int argc, char **argv)
         a = SDL_GetTicks();
         auto delta = a - b;
 
-        if (delta > 1000.0/60.0)
+        // if (delta > 1000.0/60.0)
         {
             ren.beginFrame();
 
+            sys.tick(&r1);
+            sys.draw(&r1, ren);
 
             ren.endFrame();
             b = SDL_GetTicks();
--- src/system.h
+++ src/system.h
@@ -5,30 +5,86 @@
 
 using namespace std;
 
-#define WIDTH 256
+
+#define WIDTH 500
 
 
 struct Rule
 {
     virtual vector<vector<float>> kernel() = 0;
-    virtual float activation_fn(float board[WIDTH][WIDTH]) = 0;
+    virtual float activation_fn(float x) = 0;
 };
 
 
 struct Rule1: public Rule
 {
-    vector<vector<float>> ksernel()
+    vector<vector<float>> kernel()
     {
         return {
-            {0.0f, 0.0f, 0.0f},
-            {0.0f, 0.0f, 0.0f},
-            {0.0f, 0.0f, 0.0f}
+            { 0.100, -0.100,  0.100 },
+            {-0.000,  1.000, -0.300 },
+            {-0.500, -0.100,  0.200 }
         };
     };
 
-    float activation_fn(float board[WIDTH][WIDTH])
+    float activation_fn(float x)
     {
-        return 1.0f;
+        return common::clamp(abs(x), 0.0f, 1.0f);
+    };
+};
+
+struct Rule2: public Rule
+{
+    vector<vector<float>> kernel()
+    {
+        return {
+            { 0.050, -0.250,  0.050 },
+            {-0.250,  0.850, -0.250 },
+            { 0.050, -0.250,  0.050 }
+        };
+    };
+
+    float activation_fn(float x)
+    {
+        float fn = x*x;
+
+        return common::clamp(fn, 0.0f, 1.0f);
+    };
+};
+
+struct Worms: public Rule
+{
+    vector<vector<float>> kernel()
+    {
+        return {
+            { 0.680, -0.900,  0.680 },
+            {-0.900, -0.660, -0.900 },
+            { 0.680, -0.900,  0.680 }
+        };
+    };
+
+    float activation_fn(float x)
+    {
+        float fn = -1.0f / pow(2.0f, (0.6f*pow(x, 2.0f))) + 1.0f;
+
+        return common::clamp(fn, 0.0f, 1.0f);
+    };
+};
+
+struct SlimeMold: public Rule
+{
+    vector<vector<float>> kernel()
+    {
+        return {
+            { 0.800, -0.850,  0.800 },
+            {-0.850, -0.200, -0.850 },
+            { 0.800, -0.850,  0.800 }
+        };
+    };
+
+    float activation_fn(float x)
+    {
+        return common::clamp(-1.0f / (0.89f*pow(x, 2.0f) + 1.0f) + 1.0f, 0.0f, 1.0f);
     };
 };
 
@@ -37,28 +93,99 @@ struct Rule1: public Rule
 class System
 {
 private:
-    float _data_back[WIDTH][WIDTH], _data_front[WIDTH][WIDTH];
+    float **_data_back, **_data_front;
+    float _apply_kernel(int r, int c, vector<vector<float>> kernel);
 
 public:
-    System() {  };
+    System();
     void tick(Rule *rule);
-    void draw(Renderer &renderer);
+    void draw(Rule *rule, Renderer &renderer);
 
 };
 
 
-void System::tick(Rule *rule)
+System::System()
 {
-    
+    _data_back = new float *[WIDTH];
+    _data_front = new float *[WIDTH];
+
+    for (int i=0; i<WIDTH; i++)
+    {
+        _data_back[i]  = new float [WIDTH];
+        _data_front[i] = new float [WIDTH];
+    }
+
+
+    for (int i=0; i<WIDTH; i++)
+    {
+        for (int j=0; j<WIDTH; j++)
+        {
+            _data_front[i][j] = 0.0f;
+            _data_back[i][j] = 0.0f;
+            
+            if (rand()%1000 < 1000)
+            {
+                _data_front[i][j] = (rand()%100) / 100.0f;
+                _data_back[i][j] =  (rand()%100) / 100.0f;
+            }
+        }
+    }
 }
 
 
-void System::draw(Renderer &renderer)
+
+float System::_apply_kernel(int r, int c, vector<vector<float>> kernel)
+{
+    float sum = 0.0f;
+
+    int r0 = abs(r-1)%WIDTH;
+    int r1 = abs(r+0)%WIDTH;
+    int r2 = abs(r+1)%WIDTH;
+
+    int c0 = abs(c-1)%WIDTH;
+    int c1 = abs(c+0)%WIDTH;
+    int c2 = abs(c+1)%WIDTH;
+
+    sum += kernel[0][0] * _data_front[r0][c0];
+    sum += kernel[0][1] * _data_front[r0][c1];
+    sum += kernel[0][2] * _data_front[r0][c2];
+
+    sum += kernel[1][0] * _data_front[r1][c0];
+    sum += kernel[1][1] * _data_front[r1][c1];
+    sum += kernel[1][2] * _data_front[r1][c2];
+
+    sum += kernel[2][0] * _data_front[r2][c0];
+    sum += kernel[2][1] * _data_front[r2][c1];
+    sum += kernel[2][2] * _data_front[r2][c2];
+
+    return sum;
+}
+
+
+void System::tick(Rule *rule)
 {
     for (int i=0; i<WIDTH; i++)
     {
         for (int j=0; j<WIDTH; j++)
         {
+            _data_back[i][j] = _apply_kernel(i, j, rule->kernel());
+            _data_back[i][j] = rule->activation_fn(_data_back[i][j]);
+        }
+    }
+
+    for (int i=0; i<WIDTH; i++)
+        for (int j=0; j<WIDTH; j++)
+            _data_front[i][j] = _data_back[i][j];
+}
+
+
+void System::draw(Rule *rule, Renderer &renderer)
+{
+    for (int i=0; i<WIDTH; i++)
+    {
+        for (int j=0; j<WIDTH; j++)
+        {
+            renderer.pixel(vec2(i, j), vec3(255*_data_front[i][j]));
         }
     }
 }
